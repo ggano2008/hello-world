7284f83d940dfdf4be10d6ab50c7e9bd0d6f55bc
gégé <gege@maison.moi>
2017-11-30 09:24:16 +0100

    resolution faite

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void resolution()
+{
+   printf("resolution classique\n");
+}
+
 void f1()
 {
    // yo
@@ -16,5 +21,6 @@ int main()
    printf("Bonjour and Guten tag\n");
    f1();
    f2();
+   resolution();
    return EXIT_SUCCESS;
 }
